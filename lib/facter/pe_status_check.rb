eceece0c954d8e360c129c7d4395aab5ca29421a
Taika Augustaitis <taika.augustaitis@puppet.com>
2022-03-30 11:14:48 -0400

    modified fact to be false if license file doesn't exist

--- lib/facter/pe_status_check.rb
+++ lib/facter/pe_status_check.rb
@@ -185,24 +185,31 @@ Facter.add(:pe_status_check, type: :aggregate) do
   chunk(:S0022) do
     # Is there a valid license present, which does not expire in 90 days
     # Also takes into account if the license is perpetual
+    next unless PEStatusCheck.primary?
     license_file = '/etc/puppetlabs/license.key'
-    next unless PEStatusCheck.primary? && File.exist?(license_file)
-    license_type = File.readlines(license_file).grep(%r{license_type:}).to_s
-    if license_type.include? 'Perpetual'
-      validity = true
-    elsif license_type.include? 'Subscription'
-      require 'date'
-      begin
-        end_date = Date.parse(File.readlines(license_file).grep(%r{end:}).to_s)
-        today_date = Date.today
-        daysexp = (end_date - today_date).to_i
-        validity = (today_date <= end_date) && (daysexp >= 90) ? true : false
-      rescue ArgumentError
-        # license file has missing or invalid end date
+    if File.exist?(license_file)
+      license_type = File.readlines(license_file).grep(%r{license_type:}).to_s
+      if license_type.include? 'Perpetual'
+        validity = true
+      elsif license_type.include? 'Subscription'
+        require 'date'
+        begin
+          end_date = Date.parse(File.readlines(license_file).grep(%r{end:}).to_s)
+          today_date = Date.today
+          daysexp = (end_date - today_date).to_i
+          validity = (today_date <= end_date) && (daysexp >= 90) ? true : false
+        rescue StandardError => e
+          Facter.warn("Error in fact 'pe_status_check' when checking license end date: #{e.message}")
+          Facter.debug(e.backtrace)
+          # license file has missing or invalid end date
+          validity = false
+        end
+      else
+        # license file has missing or invalid license_type
         validity = false
       end
     else
-      # license file has missing or invalid license_type
+      # license file doesn't exist
       validity = false
     end
     { S0022: validity }
